8ef18ffa3d56b8b0fc28fbed1217a0753a7a0b8d
Cifox92 <drgomez92@gmail.com>
2025-01-01 19:32:24 +0100

    Finalizado

---
+++ src/router.tsx
@@ -0,0 +1,22 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import IndexPage from './views/IndexPage'
+import FavoritesPage from './views/FavoritesPage'
+import Layout from './layouts/Layout'
+
+export default function AppRouter() {
+    return (
+        <BrowserRouter
+            future={{
+                v7_startTransition: true,
+                v7_relativeSplatPath: true
+            }}
+        >
+            <Routes>
+                <Route element={<Layout />}>
+                    <Route path='/' element={<IndexPage />} index />
+                    <Route path='/favoritos' element={<FavoritesPage />} />
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    )
+}
